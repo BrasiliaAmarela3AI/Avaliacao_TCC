746812c61fe26d9e335b507417cfb0430140c3a6
Ana <aninha@email.com>
2023-08-22 11:05:01 -0300

    atualizado

---
+++ src/main/java/br/com/etechoracio/MAGYM/Entity/Armario.java
@@ -0,0 +1,15 @@
+package br.com.etechoracio.MAGYM.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="TBL_ARMARIO")
+public class Armario {
+
+}
