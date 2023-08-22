d704d0bd1d434d090608494d8147f99bff5ff09d
Ana <aninha@email.com>
2023-08-22 10:42:33 -0300

    Inicio 22-08

---
+++ src/main/java/br/com/etechoracio/MAGYM/MagymApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.MAGYM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/MAGYM/MagymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.MAGYM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
